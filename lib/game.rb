aeb2fc8a82ec437859596183e0c8a1d3bd238eee
Monatric <nawpnawp8@gmail.com>
2024-07-30 23:03:48 +0800

    Allow user to enter  a letter

--- lib/game.rb
+++ lib/game.rb
@@ -6,18 +6,30 @@ class Game
       element.chomp.length > 5 && element.chomp.length < 12
     end.sample.chomp
     @secret_word_blank = []
+    @secret_word_reference = secret_word.chars
     @guess = []
     @attempts = 8
 
+    p secret_word_reference
     puts secret_word
     start_game
   end
 
-  attr_reader :secret_word, :guess, :secret_word_blank, :dictionary
+  attr_reader :secret_word, :guess, :secret_word_blank, :dictionary, :secret_word_reference
 
   def start_game
     puts "Welcome! Try to guess the word."
     set_up_interface
+    user_guess = gets.chomp
+    is_correct = false
+    secret_word_reference.each_with_index do |element, index|
+      if user_guess == element
+        secret_word_blank[index] = user_guess
+        is_correct = true
+      elsif secret_word_reference.size - 1 == index && is_correct == false
+        puts "Letter does not exist! Try again."
+      end
+    end
   end
 
   def set_up_interface
