260c7edfd76133b4b1f34f220c8dc06a97a08eb0
Monatric <nawpnawp8@gmail.com>
2024-07-30 22:39:32 +0800

    Display the board

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,32 @@
 # game
 class Game
   def initialize
-    dictionary = File.readlines("dictionary.txt")
-    secret_word = dictionary.select do |element|
+    @dictionary = File.readlines("dictionary.txt")
+    @secret_word = dictionary.select do |element|
       element.chomp.length > 5 && element.chomp.length < 12
-    end.sample
+    end.sample.chomp
+    @secret_word_blank = []
+    @guess = []
+    @attempts = 8
 
     puts secret_word
+    start_game
+  end
+
+  attr_reader :secret_word, :guess, :secret_word_blank, :dictionary
+
+  def start_game
+    puts "Welcome! Try to guess the word."
+    set_up_interface
+  end
+
+  def set_up_interface
+    secret_word.length.times do |n|
+      secret_word_blank.push("⎯")
+    end
+    secret_word_blank.each_with_index do |element, index|
+      print "#{element} "
+      puts "\nEnter a letter for your guess.\n" if secret_word_blank.size - 1 == index
+    end
   end
 end
