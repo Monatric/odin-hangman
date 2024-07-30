250e2171bc371fc3bd04883a833360ca6340a4c9
Monatric <nawpnawp8@gmail.com>
2024-07-31 03:42:03 +0800

    Fix

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,11 @@
+require "pry-byebug"
+
 # game
 class Game
+  DICTIONARY = File.readlines("dictionary.txt")
+
   def initialize
-    @dictionary = File.readlines("dictionary.txt")
-    @secret_word = dictionary.select do |element|
+    @secret_word = DICTIONARY.select do |element|
       element.chomp.length > 5 && element.chomp.length < 12
     end.sample.chomp
     @secret_word_blank = []
@@ -11,11 +14,10 @@ class Game
     @attempts = 8
     @chosen_letters = []
     @is_finished = false
-
-    p secret_word_reference
-    puts secret_word
     start_game
   end
+
+  private
 
   attr_accessor :is_finished, :attempts
   attr_reader :secret_word, :guess, :secret_word_blank, :dictionary, :secret_word_reference, :chosen_letters
@@ -25,7 +27,6 @@ class Game
     set_up_interface
     until is_finished == true
       check_winner
-      break if is_finished == true
 
       take_user_guess
       display_missing_word
@@ -34,12 +35,15 @@ class Game
 
   def check_winner
     if secret_word_blank == secret_word_reference
+      puts "The word is #{secret_word}"
       puts "Winner!"
       self.is_finished = true
     elsif attempts.zero?
+      puts "The word is #{secret_word}"
       puts "LOser"
       self.is_finished = true
     end
+    is_finished == true
   end
 
   def take_user_guess
@@ -66,6 +70,8 @@ class Game
         is_correct = true
       elsif secret_word_reference.size - 1 == index && is_correct == false
         self.attempts -= 1
+        break if attempts.zero?
+
         display_attempts
       end
     end
@@ -81,7 +87,10 @@ class Game
   def display_missing_word
     puts "\n"
     secret_word_blank.each_with_index do |element, index|
+      break if check_winner == true
+
       print "#{element} "
+
       print "\nEnter a letter for your guess: " if secret_word_blank.size - 1 == index
     end
   end
